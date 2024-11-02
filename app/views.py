7b8286e28f1a34667e4e7ef7cb8e1e75ce234808
1raulit <1raulit11122004@gmail.com>
2024-11-02 18:43:55 +0200

    додано ендпоінти для records

--- app/views.py
+++ app/views.py
@@ -58,6 +58,49 @@ def delete_category(category_id):
         return jsonify({'message': 'Category deleted successfully'})
     return jsonify({'error': 'Category not found'}), 404
 
+# Routes for Record
+@app.route('/record/<int:record_id>', methods=['GET'])
+def get_record(record_id):
+    record = records.get(record_id)
+    if not record:
+        return jsonify({'error': 'Record not found'}), 404
+    return jsonify(record)
+
+@app.route('/record/<int:record_id>', methods=['DELETE'])
+def delete_record(record_id):
+    if record_id in records:
+        del records[record_id]
+        return jsonify({'message': 'Record deleted successfully'})
+    return jsonify({'error': 'Record not found'}), 404
+
+@app.route('/record', methods=['POST'])
+def create_record():
+    data = request.get_json()
+    record_id = generate_id(records)
+    records[record_id] = {
+        'id': record_id,
+        'user_id': data['user_id'],
+        'category_id': data['category_id'],
+        'timestamp': datetime.today(),
+        'amount': data['amount']
+    }
+    return jsonify(records[record_id])
+
+@app.route('/record', methods=['GET'])
+def get_records():
+    user_id = request.args.get('user_id')
+    category_id = request.args.get('category_id')
+
+    if not user_id and not category_id:
+        return jsonify({'error': 'Please provide user_id or category_id or both'}), 400
+
+    filtered_records = [
+        record for record in records.values()
+        if (not user_id or record['user_id'] == int(user_id)) and
+           (not category_id or record['category_id'] == int(category_id))
+    ]
+    return jsonify(filtered_records)
+
 # Run the application
 if __name__ == '__main__':
     app.run(debug=False)
