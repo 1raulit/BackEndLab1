740603447b32dd3fa853263007186f8bbad45d70
1raulit <1raulit11122004@gmail.com>
2024-12-11 17:08:14 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app import views
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile("config.py", silent=True)
+
+    # Ініціалізація розширень
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Реєстрація Blueprints
+    from app.routes import income_routes
+    app.register_blueprint(income_routes.income_bp)
+    from app.routes import expense_routes
+    app.register_blueprint(expense_routes.expense_bp)
+    from app.routes import account_routes
+    app.register_blueprint(account_routes.account_bp)
+
+    return app
